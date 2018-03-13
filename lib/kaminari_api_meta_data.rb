42dd958173a35da39ecb8276677d6a8e400cf212
Stuart <stuart.chinery@gmail.com>
2018-03-13 12:10:40 +0000

    Bumped Ruby version

--- lib/kaminari_api_meta_data.rb
+++ lib/kaminari_api_meta_data.rb
@@ -1,7 +1,6 @@
 require "kaminari_api_meta_data/version"
 
 module KaminariApiMetaData
-
   def meta_data(collection, extra_meta = {})
     {
       current_page: collection.current_page,
@@ -12,5 +11,4 @@ module KaminariApiMetaData
       total_count: collection.total_count
     }.merge(extra_meta)
   end
-
 end
