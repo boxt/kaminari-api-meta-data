49d99e04ecd826895c4e08cb60b702739dd8e8d0
Stuart Chinery <stuart.chinery@gmail.com>
2020-09-25 08:16:55 +0100

    Cleaned up specs

--- spec/kaminari_api_meta_data_spec.rb
+++ spec/kaminari_api_meta_data_spec.rb
@@ -1,44 +1,50 @@
 # frozen_string_literal: true
 
-require("spec_helper")
+require "spec_helper"
 
 class Dummy
   include(::KaminariApiMetaData)
 end
 
-describe("KaminariApiMetaDataTest") do
-  let(:dummy) { Dummy.new }
+describe KaminariApiMetaData do
+  describe "VERSION" do
+    let(:current_version) { File.read("VERSION").split("\n").first }
 
-  let(:kaminari_collection) do
-    OpenStruct.new(
-      current_page: 1,
-      next_page: 2,
-      limit_value: 20,
-      prev_page: nil,
-      total_pages: 10,
-      total_count: 23
-    )
+    it "is set from the VERSION file" do
+      expect(::KaminariApiMetaData::VERSION).to eq(current_version)
+    end
   end
 
-  it("has a version") { expect(::KaminariApiMetaData::VERSION).to(be_truthy) }
+  describe "#meta_data(collection, extra_meta = {})" do
+    let(:dummy) { Dummy.new }
 
-  describe("#meta_data(collection, extra_meta = {})") do
-    describe("with no extra meta data") do
+    let(:kaminari_collection) do
+      OpenStruct.new(
+        current_page: 1,
+        next_page: 2,
+        limit_value: 20,
+        prev_page: nil,
+        total_pages: 10,
+        total_count: 23
+      )
+    end
+
+    describe "with no extra meta data" do
       let(:expected) do
         { current_page: 1, next_page: 2, per_page: 20, prev_page: nil, total_pages: 10, total_count: 23 }
       end
 
-      it("returns meta attributes") do
+      it "returns meta attributes" do
         expect(dummy.meta_data(kaminari_collection)).to(eq(expected))
       end
     end
 
-    describe("with extra meta data") do
+    describe "with extra meta data" do
       let(:expected) do
         { current_page: 1, next_page: 2, per_page: 20, prev_page: nil, total_pages: 10, total_count: 23, foo: "bar" }
       end
 
-      it("returns meta attributes") do
+      it "returns meta attributes" do
         expect(dummy.meta_data(kaminari_collection, foo: "bar")).to(eq(expected))
       end
     end
